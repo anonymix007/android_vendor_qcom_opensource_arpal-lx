06853fd9ad6edb39cb084052b159848aa07b77b4
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-10-02 21:31:48 +0300

    pal: Add kvh2xml_headers dependency

--- Android.mk
+++ Android.mk
@@ -144,7 +144,8 @@ LOCAL_HEADER_LIBRARIES := \
     libagm_headers \
     libacdb_headers \
     libarosal_headers \
-    libvui_dmgr_headers
+    libvui_dmgr_headers \
+    kvh2xml_headers
 
 LOCAL_SHARED_LIBRARIES := \
     libar-gsl\
